33eecd68566e36546a9f2271b005292e99ffb5c1
Elizabeth Craig <elcraig@microsoft.com>
2020-05-28 10:33:52 -0700

    feat: catch more deprecation cases and add tests

---
+++ tests/fixtures/deprecatedExports.ts
@@ -0,0 +1,11 @@
+// This file is not actually tested, but it's "imported" from in some tests.
+
+/** @deprecated */
+export interface Interface1 {}
+
+/** @deprecated */
+export const def1 = 3;
+
+/** @deprecated deprecated default export */
+let def2 = '';
+export default def2;
---
+++ tests/fixtures/file.ts
@@ -0,0 +1,2 @@
+// This is a fake filename used by the test harness when parsing test cases.
+// A file must exist at this location to make parsing work.
