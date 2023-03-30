d7172422d83067f045acd2398f6489ffeb8e3ec8
Alex Malkevich <malkevich.alex@gmail.com>
2023-03-31 01:13:15 +0200

    ci(ga): fix TS v3 BC check

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,21 @@
+const BC_MODE = !!process.env.BC_MODE;
+
 module.exports = {
   testEnvironment: 'node',
   transform: {
-    '^.+\\.tsx?$': ['ts-jest', {
-      isolatedModules: true,
-    }],
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        isolatedModules: true,
+      },
+    ],
   },
   testRegex: './tests/.+\\.test\\.ts$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  moduleNameMapper: BC_MODE
+    ? {
+        '@eslint/eslintrc/universal':
+          '@eslint/eslintrc/dist/eslintrc-universal.cjs',
+      }
+    : undefined,
 };
